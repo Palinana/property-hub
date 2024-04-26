8d0efc37d26ff9b609e229cffdc9495beca9567b
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2024-04-26 16:41:25 -0400

    Added info boxes component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 import Hero from '@/components/Homepage/Hero';
+import InfoBoxes from '@/components/Homepage/InfoBoxes';
 
 const HomePage = () => {
   return (
     <>
         <Hero />
+        <InfoBoxes />
     </>
   )
 }
