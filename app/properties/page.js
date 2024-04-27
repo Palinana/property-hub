a8f47db509454e6dedce7faec8b911eabc7413e3
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2024-04-27 17:53:24 -0400

    Added property cards and data for rentals

--- app/properties/page.js
+++ app/properties/page.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import Link from 'next/link';
+import properties from '@/properties.json'
+import PropertyCard from '@/components/Property/PropertyCard'
 
 const PropertiesPage = () => {
     return (
-        <div>
-            <h1 className='text-3xl'>Properties</h1>
-            <Link href='/'>Go home</Link>
-        </div>
+        <>
+            <section className='bg-blue-700 py-4'>
+                <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
+                </div>
+            </section>
+
+                <section className='px-4 py-6'>
+                <div className='container-xl lg:container m-auto px-4 py-6'>
+                    {properties.length === 0 ? (
+                        <p>No properties found</p>
+                    ) : (
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                        {properties.map((property) => (
+                            <PropertyCard key={property._id} property={property} />
+                        ))}
+                    </div>
+                    )}
+                </div>
+            </section>
+        </>
     )
 }
 
