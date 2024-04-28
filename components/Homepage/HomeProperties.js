75ede738bb9b2b83e39429d3459a68a64d43ce4b
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2024-04-27 22:20:44 -0400

    Added link to all listings from homepage

--- components/Homepage/HomeProperties.js
+++ components/Homepage/HomeProperties.js
@@ -26,6 +26,15 @@ const HomeProperties = async () => {
                     </div>
                 </div>
             </section>
+
+            <section className='m-auto max-w-lg my-10 px-6'>
+                <Link
+                    href='/properties'
+                    className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'
+                >
+                    View All Properties
+                </Link>
+            </section>
         </>
     )
 }
