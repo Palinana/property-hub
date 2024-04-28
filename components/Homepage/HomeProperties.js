201ae3e1bcb478393136bf8c01b7e7cd93d0728d
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2024-04-27 22:19:07 -0400

    Minimized the list of properties for the homepage to 3 random listings

--- components/Homepage/HomeProperties.js
+++ components/Homepage/HomeProperties.js
@@ -3,6 +3,11 @@ import PropertyCard from '@/components/Property/PropertyCard';
 import properties from '@/properties.json'
 
 const HomeProperties = async () => {
+    // to display only 3 random property
+    const recentProperties = properties
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 3);
+
     return (
         <>
             <section className='px-4 py-6'>
@@ -14,7 +19,7 @@ const HomeProperties = async () => {
                         {recentProperties.length === 0 ? (
                             <p>No Properties Found</p>
                             ) : (
-                            recentProperties.map((property) => (
+                                recentProperties.map((property) => (
                                 <PropertyCard key={property._id} property={property} />
                             ))
                         )}
