45348e9899c1560359f89b9ceeef7395f292a147
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2024-04-27 22:18:05 -0400

    Added home property component for the home page

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Link from 'next/link';
 import Hero from '@/components/Homepage/Hero';
 import InfoBoxes from '@/components/Homepage/InfoBoxes';
+import HomeProperties from '@/components/Homepage/HomeProperties';
 
 const HomePage = () => {
   return (
     <>
         <Hero />
         <InfoBoxes />
+        <HomeProperties />
     </>
   )
 }
---
+++ components/Homepage/HomeProperties.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import PropertyCard from '@/components/Property/PropertyCard';
+import properties from '@/properties.json'
+
+const HomeProperties = async () => {
+    return (
+        <>
+            <section className='px-4 py-6'>
+                <div className='container-xl lg:container m-auto'>
+                    <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
+                        Recent Properties
+                    </h2>
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                        {recentProperties.length === 0 ? (
+                            <p>No Properties Found</p>
+                            ) : (
+                            recentProperties.map((property) => (
+                                <PropertyCard key={property._id} property={property} />
+                            ))
+                        )}
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default HomeProperties;
