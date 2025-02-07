496879f3a5ffe85874dedc6b289858f6298758b3
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2025-02-07 10:01:08 -0500

    Implemented submission of new user listing to database

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,7 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/utils/authOptions';
 
 // GET /api/properties
 export const GET = async (request) => {
@@ -14,5 +16,63 @@ export const GET = async (request) => {
     } catch (error) {
         console.log(error);
         return new Response('Something Went Wrong', { status: 500 });
+    }
+};
+
+export const POST = async (request) => {
+    try {
+        await connectDB();
+
+        const session = await getServerSession(authOptions);
+
+        if (!session) {
+            return new Response('Unauthorized', { status: 401 });
+        }
+        
+        const userId = session.user.id;
+        console.log('userId --> ', userId)
+
+        const formData = await request.formData();
+
+        // Access all values from amenities and images
+        const amenities = formData.getAll('amenities');
+        const images = formData
+            .getAll('images')
+            .filter((image) => image.name !== ''); // only if images were uploaded
+
+        // Create propertyData object for database and set values given in the form
+        const propertyData = {
+            type: formData.get('type'),
+            name: formData.get('name'),
+            description: formData.get('description'),
+            location: {
+                street: formData.get('location.street'),
+                city: formData.get('location.city'),
+                state: formData.get('location.state'),
+                zipcode: formData.get('location.zipcode'),
+            },
+            beds: formData.get('beds'),
+            baths: formData.get('baths'),
+            square_feet: formData.get('square_feet'),
+            amenities,
+            rates: {
+                weekly: formData.get('rates.weekly'),
+                monthly: formData.get('rates.monthly'),
+                nightly: formData.get('rates.nightly.'),
+            },
+            seller_info: {
+                name: formData.get('seller_info.name'),
+                email: formData.get('seller_info.email'),
+                phone: formData.get('seller_info.phone'),
+            },
+            owner: userId,
+        };
+
+
+        return new Response(JSON.stringify({ message: 'Success' }), {
+            status: 200,
+        })
+    } catch (error) {
+        return new Response('Failed to add property', { status: 500 });
     }
 };
