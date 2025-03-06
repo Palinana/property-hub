f87a928c89bc36eb1e8054cafc8f8a4ed3a4986d
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2025-03-05 19:06:00 -0500

    Update search form

--- app/properties/search-results/page.js
+++ app/properties/search-results/page.js
@@ -1,62 +1,62 @@
-'use client';
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation'; 
 import Link from 'next/link';
 import { FaArrowAltCircleLeft } from 'react-icons/fa';
 import PropertyCard from '@/components/Property/PropertyCard';
-import Spinner from '@/components/Elements/Spinner';
 import PropertySearchForm from '@/components/Property/PropertySearchForm';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+import { convertToSerializeableObject } from '../../../utils/convertToObject';
 
-const SearchResultsPage = async () => {
-    const searchParams = useSearchParams();
+const SearchResultsPage = async ({ searchParams: { location, propertyType }}) => {
+    await connectDB();
 
-    const [properties, setProperties] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const locationPattern = new RegExp(location, 'i');
 
-    const location = searchParams.get('location');
-    const propertyType = searchParams.get('propertyType');
+    // Match location pattern against database fields
+    let query = {
+        $or: [
+            { name: locationPattern },
+            { description: locationPattern },
+            { 'location.street': locationPattern },
+            { 'location.city': locationPattern },
+            { 'location.state': locationPattern },
+            { 'location.zipcode': locationPattern },
+        ],
+    };
 
-    useEffect(() => {
-        const fetchSearchResults = async () => {
-            try {
-                const res = await fetch(`/api/properties/search?location=${location}&propertyType=${propertyType}`);
+    // Only check for property if its not 'All'
+    if (propertyType && propertyType !== 'All') {
+        const typePattern = new RegExp(propertyType, 'i');
+        query.type = typePattern;
+    }
 
-                if (res.status === 200) {
-                    const data = await res.json();
-                    setProperties(data);
-                }
-                else {
-                    console.log(res.statusText);
-                    toast.error('Failed to fetch saved properties');
-                }
-            } catch (error) {
-                console.log(error);
-                toast.error('Failed to fetch saved properties');
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        fetchSearchResults();
-    }, [location, propertyType]);
+    const propertiesQueryResults = await Property.find(query).lean();
+    const properties = propertiesQueryResults.map(convertToSerializeableObject);
 
     return (
         <>
             <section className='bg-blue-700 py-4'>
                 <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
+                    <PropertySearchForm />
                 </div>
             </section>
-
+            
             <section className='px-4 py-6'>
                 <div className='container-xl lg:container m-auto px-4 py-6'>
+                    <Link
+                        href='/properties'
+                        className='flex items-center text-blue-500 hover:underline mb-3'
+                    >
+                        <FaArrowAltCircleLeft className='mr-2 mb-1' /> Back To Properties
+                    </Link>
+                    <h1 className='text-2xl mb-4'>Search Results</h1>
                     {properties.length === 0 ? (
-                        <p>No properties found</p>
+                        <p>No search results found</p>
                     ) : (
-                    <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
                         {properties.map((property) => (
                             <PropertyCard key={property._id} property={property} />
                         ))}
-                    </div>
+                        </div>
                     )}
                 </div>
             </section>
